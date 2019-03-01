ba6d38e48cbfb61c8b85a8e30a1ed8668862c262
Victor J. Owusu <jowusu837@gmail.com>
2019-03-01 14:12:51 +0000

    Added migrations for a start. Some of this might change as we progress.

---
+++ database/migrations/2019_03_01_140723_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('survey_id')->index();
+            $table->string('question');
+            $table->text('options')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}

78bd2d3e66d23a46aca7fec46abab2a0bfda43c4
Victor J. Owusu <jowusu837@gmail.com>
2019-03-02 18:47:11 +0000

    Added test for create survey feature

--- tests/Feature/SurveyTest.php
+++ tests/Feature/SurveyTest.php
@@ -13,24 +13,63 @@ class SurveyTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * A user should be able to see a list of surveys he or she has created
+     * Our test user
+     * @var User
+     */
+    private $user;
+
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // We will be authenticating as this user
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * This helper function helps us to be dry
+     * @return SurveyTest
+     */
+    private function actingAsUser(): SurveyTest
+    {
+        return $this->actingAs($this->user);
+    }
+
+    /**
+     * This test is supposed to guarantee that when we create surveys,
+     * they get parsed through the controller to the view.
      *
      * @return void
      */
     public function testListing()
     {
-        // Let's create a user with some surveys
-        $user = factory(User::class)->create();
+        $surveys = $this->user->surveys()->saveMany(factory(Survey::class, 5)->make());
 
-        $surveys = $user->surveys()->saveMany(factory(Survey::class, 5)->make());
-
-        $response = $this->actingAs($user)
+        $response = $this->actingAsUser()
             ->get(route('surveys.index'))
             ->assertSuccessful()
+            ->assertViewIs('surveys.index')
             ->assertViewHas('surveys');
 
-        $viewData = $this->getResponseData($response, 'surveys');
+        // Since we are not paginating, let's ensure that we see all our surveys on the page
+        foreach ($surveys as $survey) {
+            $response->assertSee($survey->title);
+        }
+    }
 
-        $this->assertEquals(count($viewData), count($surveys));
+    /**
+     * We want to test that our view for creating a survey loads as expected.
+     * Since the actual creation happens via API, we'll not cover that here.
+     *
+     * Also, we want to ensure that our react application node is there.
+     */
+    public function testCreate()
+    {
+        $this->actingAsUser()
+            ->get(route('surveys.create'))
+            ->assertSuccessful()
+            ->assertViewIs('surveys.create')
+            ->assertSee('<div id="create-survey"></div>');
     }
 }
